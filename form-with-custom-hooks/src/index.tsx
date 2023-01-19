411c4f442a08737e6feb9b68bb15f8bd3de9cbec
Elina McGill <elinamcgill@gmail.com>
2023-01-19 11:07:14 -0500

    Implement type checking

--- form-with-custom-hooks/src/index.tsx
+++ form-with-custom-hooks/src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client.js';
 import './Form.css';
 import Form from './Form';
 import reportWebVitals from './reportWebVitals';
 
+const docElement = document.querySelector('#root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Form />
-  </React.StrictMode>
-);
+if (docElement) {
+  const root = ReactDOM.createRoot(docElement);
+  root.render(
+    <React.StrictMode>
+      <Form />
+    </React.StrictMode>,
+  )
+} else console.log("ERROR: Root element is null / cannot be found/");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
